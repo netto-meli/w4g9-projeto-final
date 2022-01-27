bddd2075e8b1763b04bc878b2cadb74faa021614
netto-meli <fernando.netto@mercadolivre.com>
2022-01-26 21:56:56 -0300

    Criação Banco de dados

---
+++ src/main/java/com/mercadolibre/fernando_netto_projeto_final/controller/WarehouseController.java
@@ -0,0 +1,20 @@
+package com.mercadolibre.fernando_netto_projeto_final.controller;
+
+import com.mercadolibre.fernando_netto_projeto_final.entity.Warehouse;
+import com.mercadolibre.fernando_netto_projeto_final.service.WarehouseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WarehouseController {
+
+    @Autowired
+    WarehouseService warehouseService;
+
+    @GetMapping("/api")
+    public Warehouse api() {
+        Warehouse wh = new Warehouse(1L, "ziks");
+        return warehouseService.save(wh);
+    }
+}
