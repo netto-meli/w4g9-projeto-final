9d90278576989e6d3635a8bf017ebdc5adff2053
Felipe Bontempo <felipe.bontempo@mercadolivre.com>
2022-01-28 11:15:06 -0300

    Metodo POST - Cadastrar Section

--- src/main/java/com/mercadolibre/w4g9projetofinal/controller/SectionController.java
+++ src/main/java/com/mercadolibre/w4g9projetofinal/controller/SectionController.java
@@ -9,3 +9,12 @@
         List sections = sectionRepository.findAll();
         return SectionRequestDTO.convert(sections);
     }
+
+    @PostMapping
+    public ResponseEntity<SectionResponseDTO> cadastrar(@RequestBody SectionRequestDTO sectionRequestDTO,
+                                                        UriComponentsBuilder uriBuilder){
+        Section section = sectionRequestDTO.convertToSection(warehouseRepository);
+        sectionRepository.save(section);
+        URI uri = uriBuilder.path("/section/{id}").buildAndExpand(section.getId()).toUri();
+        return ResponseEntity.created(uri).body(new SectionResponseDTO(section));
+    }
