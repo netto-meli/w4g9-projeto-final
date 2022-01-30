7782ccf555dd0958e89fe1e59c2540e98d74680c
netto-meli <fernando.netto@mercadolivre.com>
2022-01-29 21:16:22 -0300

    merge Leonardo

--- src/main/java/com/mercadolibre/w4g9projetofinal/repository/AdvertiseRepository.java
+++ src/main/java/com/mercadolibre/w4g9projetofinal/repository/AdvertiseRepository.java
@@ -4,8 +4,6 @@ import com.mercadolibre.w4g9projetofinal.entity.Advertise;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
 public interface AdvertiseRepository extends JpaRepository<Advertise, Long> {
-
 }
