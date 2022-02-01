38fea83a416997f93d35b664727a6d190776932b
Felipe Bontempo <felipe.bontempo@mercadolivre.com>
2022-02-01 16:36:42 -0300

    Limpeza de imports na classe SectionRequestDTO

--- src/main/java/com/mercadolibre/w4g9projetofinal/dtos/request/SectionRequestDTO.java
+++ src/main/java/com/mercadolibre/w4g9projetofinal/dtos/request/SectionRequestDTO.java
@@ -4,14 +4,9 @@ import com.mercadolibre.w4g9projetofinal.entity.Section;
 import com.mercadolibre.w4g9projetofinal.entity.Warehouse;
 import com.mercadolibre.w4g9projetofinal.entity.enums.RefrigerationType;
 import com.mercadolibre.w4g9projetofinal.repository.SectionRepository;
-import com.mercadolibre.w4g9projetofinal.repository.WarehouseRepository;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 /**
  * @author fbontempo
  * @version 0.2
