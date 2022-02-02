286dcb5cee50ad3ed8136809c9257e32550f4bf0
netto-meli <fernando.netto@mercadolivre.com>
2022-02-02 14:51:49 -0300

    Validation Handler

--- src/main/java/com/mercadolibre/w4g9projetofinal/advice/ResourceExceptionHandler.java
+++ src/main/java/com/mercadolibre/w4g9projetofinal/advice/ResourceExceptionHandler.java
@@ -3,6 +3,8 @@ package com.mercadolibre.w4g9projetofinal.advice;
 import com.mercadolibre.w4g9projetofinal.exceptions.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -16,6 +18,20 @@ import java.nio.file.AccessDeniedException;
  */
 @ControllerAdvice
 public class ResourceExceptionHandler {
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+		ValidationError err = new ValidationError(
+				System.currentTimeMillis(),
+				HttpStatus.UNPROCESSABLE_ENTITY.value(),
+				"Erro de Validação: ",
+				request.getRequestURI(),
+				e.getMessage());
+		for (FieldError x : e.getBindingResult().getFieldErrors()) {
+			err.addError(x.getField(), x.getDefaultMessage());
+		}
+		return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(err);
+	}
 
 	/*** Handler de erro
 	 *
