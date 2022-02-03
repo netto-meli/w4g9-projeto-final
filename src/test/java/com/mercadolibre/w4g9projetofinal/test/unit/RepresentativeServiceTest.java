0c323a845a785f0b830f2513106f80680f43b62d
Marcos Sa <marcos.sa@mercadolivre.com>
2022-02-03 18:48:13 -0300

    Atualização em nas classes de testes Unitários

--- src/test/java/com/mercadolibre/w4g9projetofinal/test/unit/RepresentativeServiceTest.java
+++ src/test/java/com/mercadolibre/w4g9projetofinal/test/unit/RepresentativeServiceTest.java
@@ -3,6 +3,7 @@ package com.mercadolibre.w4g9projetofinal.test.unit;
 import com.mercadolibre.w4g9projetofinal.entity.Representative;
 import com.mercadolibre.w4g9projetofinal.entity.Seller;
 import com.mercadolibre.w4g9projetofinal.entity.enums.RepresentativeJob;
+import com.mercadolibre.w4g9projetofinal.exceptions.ExistingUserException;
 import com.mercadolibre.w4g9projetofinal.exceptions.ObjectNotFoundException;
 import com.mercadolibre.w4g9projetofinal.repository.RepresentativeRepository;
 import com.mercadolibre.w4g9projetofinal.repository.SellerRepository;
@@ -10,10 +11,12 @@ import com.mercadolibre.w4g9projetofinal.service.RepresentativeService;
 import com.mercadolibre.w4g9projetofinal.service.SellerService;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -68,6 +71,57 @@ public class RepresentativeServiceTest {
 
         //Assertation
         assertEquals(r2, representative);
+        assertTrue(excesaoEsperada.getMessage().equals("Representante não encontrado! Por favor verifique o id."));
+    }
+
+    @Test
+    public void verificaInsertRepresentative() {
+        //arrange
+        Representative r1 = new Representative(1L, "felipe.13sd3", "Marcos Sá", "email1@gmail.com", "151515", RepresentativeJob.LIDER);
+        Representative r2 = new Representative(2L, "felipe.13dsd3", "Marcos Sá", "email2@gmail.com", "151515", RepresentativeJob.SUPERVISOR);
+
+        RepresentativeRepository mockRepresentativeRepository = Mockito.mock(RepresentativeRepository.class);
+        Mockito.when(mockRepresentativeRepository.save(r2)).thenReturn(r2);
+        Mockito.when(mockRepresentativeRepository.save(r1)).thenThrow(DataIntegrityViolationException.class);
+
+        RepresentativeService representativeService = new RepresentativeService(new BCryptPasswordEncoder(), mockRepresentativeRepository);
+
+        //Action
+        Representative representative = representativeService.insert(r2);
+
+        ExistingUserException expectedException = assertThrows(ExistingUserException.class, () -> representativeService.insert(r1));
+
+        //Assertation
+        assertEquals(r2, representative);
+        assertTrue(expectedException.getMessage().equals("Username ou Email existente na base de dados"));
+    }
+
+    @Test
+    public void verificaUpdateRepresentative() {
+
+        //Arrange
+        Representative r1 = new Representative(1L, "felipe.13sd3", "Marcos Sá", "email1@gmail.com", "151515", RepresentativeJob.LIDER);
+        Representative r2 = new Representative(2L, "felipe.13dsd3", "Marcos Sá", "email2@gmail.com", "151515", RepresentativeJob.SUPERVISOR);
+
+        Representative representativeUpdate = new Representative(r2.getId(), r2.getUsername(), "Marcos de Sá", r2.getEmail(), r2.getPassword(), r2.getJob());
+
+        RepresentativeService mockRepresentativeService = Mockito.mock(RepresentativeService.class);
+        Mockito.when(mockRepresentativeService.update(representativeUpdate)).thenReturn(representativeUpdate);
+
+        RepresentativeRepository mockRepresentativeRepository = Mockito.mock(RepresentativeRepository.class);
+        Mockito.when(mockRepresentativeRepository.save(representativeUpdate)).thenReturn(representativeUpdate);
+        Mockito.when(mockRepresentativeRepository.findById(2L)).thenReturn(java.util.Optional.of(r2));
+
+        RepresentativeService representativeService = new RepresentativeService(new BCryptPasswordEncoder(), mockRepresentativeRepository);
+
+        //Action
+        Representative representative = representativeService.update(representativeUpdate);
+        ObjectNotFoundException excesaoEsperada = assertThrows(ObjectNotFoundException.class,() -> representativeService.findById(3L));
+
+        //Action
+
+
+        assertEquals(representativeUpdate, representative);
         assertTrue(excesaoEsperada.getMessage().equals("Representante não encontrado! Por favor verifique o id."));
     }
 
--- src/test/java/com/mercadolibre/w4g9projetofinal/test/unit/SellerServiceTest.java
+++ src/test/java/com/mercadolibre/w4g9projetofinal/test/unit/SellerServiceTest.java
@@ -1,15 +1,20 @@
 package com.mercadolibre.w4g9projetofinal.test.unit;
 
 import com.mercadolibre.w4g9projetofinal.entity.Seller;
+import com.mercadolibre.w4g9projetofinal.exceptions.ExistingUserException;
 import com.mercadolibre.w4g9projetofinal.exceptions.ObjectNotFoundException;
 import com.mercadolibre.w4g9projetofinal.repository.SellerRepository;
 import com.mercadolibre.w4g9projetofinal.service.SellerService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -48,7 +53,7 @@ public class SellerServiceTest {
         Seller s2 = new Seller(2L, "felipe.133", "Felipe Bontempo", "email2@hotmail.com", "123456", null);
 
         SellerRepository mockSellerRepository = Mockito.mock(SellerRepository.class);
-        Mockito.when(mockSellerRepository.findById(2L)).thenReturn(java.util.Optional.of(new Seller(2L, "felipe.133", "Felipe Bontempo", "email2@hotmail.com", "123456", null)));
+        Mockito.when(mockSellerRepository.findById(2L)).thenReturn(java.util.Optional.of(s2));
 
         SellerService sellerService = new SellerService(null, mockSellerRepository);
 
@@ -61,5 +66,55 @@ public class SellerServiceTest {
         assertEquals(s2, seller);
         assertTrue(excesaoEsperada.getMessage().equals("Vendedor não encontrado! Por favor verifique o id."));
     }
+
+    @Test
+    public void verificaInsertSeller() {
+        //arrange
+        Seller s1 = new Seller(3L, "felipe3", "Fe Bontempo", "email2@hotmail.com", "123456", null);
+        Seller s2 = new Seller(2L, "felipe.133", "Felipe Bontempo", "email2@hotmail.com", "123456", null);
+
+        SellerRepository mockSellerRepository = Mockito.mock(SellerRepository.class);
+        Mockito.when(mockSellerRepository.save(s2)).thenReturn(s2);
+        Mockito.when(mockSellerRepository.save(s1)).thenThrow(DataIntegrityViolationException.class);
+
+        SellerService sellerService = new SellerService(new BCryptPasswordEncoder(), mockSellerRepository);
+
+        //Action
+        Seller seller = sellerService.insert(s2);
+
+        ExistingUserException expectedException = assertThrows(ExistingUserException.class, () -> sellerService.insert(s1));
+
+        //Assertation
+        assertEquals(s2, seller);
+        assertTrue(expectedException.getMessage().equals("Username ou Email existente na base de dados"));
+    }
+
+    @Test
+    public void verificaUpdateSeller() {
+
+        //Arrange
+        Seller s1 = new Seller(3L, "felipe3", "Fe Bontempo", "email2@hotmail.com", "123456", null);
+        Seller s2 = new Seller(2L, "felipe.133", "Felipe Bontempo", "email2@hotmail.com", "123456", null);
+
+        Seller sellerUpdate = new Seller(s1.getId(), s1.getUsername(), "Marcos de Sá", s1.getEmail(), s1.getPassword(), null);
+
+        SellerService mockSellerService = Mockito.mock(SellerService.class);
+        Mockito.when(mockSellerService.update(sellerUpdate)).thenReturn(sellerUpdate);
+
+        SellerRepository mockSellerRepository = Mockito.mock(SellerRepository.class);
+        Mockito.when(mockSellerRepository.save(sellerUpdate)).thenReturn(sellerUpdate);
+        Mockito.when(mockSellerRepository.findById(3L)).thenReturn(java.util.Optional.of(s1));
+
+        SellerService sellerService = new SellerService(new BCryptPasswordEncoder(), mockSellerRepository);
+
+        //Action
+        Seller seller = sellerService.update(sellerUpdate);
+        ObjectNotFoundException excesaoEsperada = assertThrows(ObjectNotFoundException.class,() -> sellerService.findById(1L));
+
+        //Action
+        assertEquals(sellerUpdate, seller);
+        assertTrue(excesaoEsperada.getMessage().equals("Vendedor não encontrado! Por favor verifique o id."));
+    }
+
 
 }
