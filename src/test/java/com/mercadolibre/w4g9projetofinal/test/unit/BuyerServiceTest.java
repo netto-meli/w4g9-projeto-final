3be45015912ab672ad97310fc3c5a01daf27fd9f
lassuncao <leonardo.assuncao@mercadolivre.com>
2022-02-04 11:39:36 -0300

    test example

--- src/test/java/com/mercadolibre/w4g9projetofinal/test/unit/BuyerServiceTest.java
+++ src/test/java/com/mercadolibre/w4g9projetofinal/test/unit/BuyerServiceTest.java
@@ -6,26 +6,33 @@ import com.mercadolibre.w4g9projetofinal.service.BuyerService;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class BuyerServiceTest {
 
     @Test
     public void verifyBuyerList() {
+        List<Buyer> b = new ArrayList<>();
 
         Buyer buyer = new Buyer(2L, "Test", "Buyer", "leota@gmail.com", "end","rua");
         Buyer buyer2 = new Buyer(4L, "Test2", "Buyer2", "leota@gmail.com", "end2","ruo");
 
+        b.add(buyer);
+        b.add(buyer2);
+
         BuyerRepository mockBuyerRepository = Mockito.mock(BuyerRepository.class);
-        Mockito.when(mockBuyerRepository.findById(4L)).thenReturn(Optional.of(buyer2));
+        Mockito.when(mockBuyerRepository.findAll()).thenReturn(b);
 
         BuyerService buyerService = new BuyerService(null, mockBuyerRepository);
 
-        Buyer buyerFind = (Buyer) buyerService.findAll();
+        List<Buyer> buyerFind = buyerService.findAll();
 
-        assertEquals(buyer, buyerFind);
+        assertEquals(b, buyerFind);
+        assertNotNull(buyerService);
     }
 
 
