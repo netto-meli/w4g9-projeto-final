81feba6a752b71046f5cd5a6658f766ffd8b1c99
lassuncao <leonardo.assuncao@mercadolivre.com>
2022-02-07 10:31:05 -0300

    resolucao de conflitos

--- src/main/java/com/mercadolibre/w4g9projetofinal/dtos/request/AdvertiseRequestDTO.java
+++ src/main/java/com/mercadolibre/w4g9projetofinal/dtos/request/AdvertiseRequestDTO.java
@@ -5,10 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 
 @Data
